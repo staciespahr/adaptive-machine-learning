8bfc3d7a1041300fa129e21c3eb86dba0e5f3182
Charles Ganu <charlesganu2004@gmail.com>
2026-06-14 19:16:56 -0400

    commit-M

---
+++ src/BB84_code_circuit/BB84.py
@@ -0,0 +1,5 @@
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, transpile
+from qiskit_aer import Aer
+from qiskit.visualization import plot_histogram
+import random
+import numpy as np
